387e18fd02f1df37d70e636b09afa235d35acf5d
Guillaume <guillaume@wechantloup.com>
2023-01-27 10:19:28 +0100

    Work with Sega MD !

--- code/gamepad.py
+++ code/gamepad.py
@@ -24,6 +24,19 @@ BTN_Z_R = 11
 BTN_13 = 12
 BTN_14 = 13
 
+IO_UP = 3
+IO_DOWN = 5
+IO_LEFT = 7
+IO_RIGHT = 8
+IO_BTN_START = 10
+IO_BTN_MODE_SELECT = 11
+IO_BTN_A = 12
+IO_BTN_B = 13
+IO_BTN_C_L = 15
+IO_BTN_X = 16
+IO_BTN_Y = 18
+IO_BTN_Z_R = 19
+
 NOT_CONNECTED = 0b000
 MEGADRIVE = 0b001
 SUPER_NES = 0b010
@@ -36,6 +49,60 @@ Pad3Bcontroller = { "1" : BTN_B, "2" : BTN_A, "5" : BTN_C_L, "9" : BTN_START, "0
 pad = {}
 
 joysticks = {}
+
+def initGpio():
+  GPIO.setup(IO_UP, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_DOWN, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_LEFT, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_RIGHT, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_BTN_START, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_BTN_MODE_SELECT, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_BTN_A, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_BTN_B, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_BTN_C_L, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_BTN_X, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_BTN_Y, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(IO_BTN_Z_R, GPIO.OUT, initial=GPIO.LOW)
+
+def getBtnGpio(btn):
+  if btn == BTN_START:
+    return IO_BTN_START
+  elif btn == BTN_MODE_SELECT:
+    return IO_BTN_MODE_SELECT
+  elif btn == BTN_A:
+    return IO_BTN_A
+  elif btn == BTN_B:
+    return IO_BTN_B
+  elif btn == BTN_C_L:
+    return IO_BTN_C_L
+  elif btn == BTN_X:
+    return IO_BTN_X
+  elif btn == BTN_Y:
+    return IO_BTN_Y
+  elif btn == BTN_Z_R:
+    return IO_BTN_Z_R
+  else:
+    return -1
+
+def performIoAction(value):
+  if value == LEFT:
+      GPIO.output(IO_LEFT, GPIO.HIGH)
+      GPIO.output(IO_RIGHT, GPIO.LOW)
+  elif value == RIGHT:
+      GPIO.output(IO_LEFT, GPIO.LOW)
+      GPIO.output(IO_RIGHT, GPIO.HIGH)
+  elif value == RELEASE_H:
+      GPIO.output(IO_LEFT, GPIO.LOW)
+      GPIO.output(IO_RIGHT, GPIO.LOW)
+  elif value == UP:
+      GPIO.output(IO_UP, GPIO.HIGH)
+      GPIO.output(IO_DOWN, GPIO.LOW)
+  elif value == DOWN:
+      GPIO.output(IO_UP, GPIO.LOW)
+      GPIO.output(IO_DOWN, GPIO.HIGH)
+  elif value == RELEASE_V:
+      GPIO.output(IO_UP, GPIO.LOW)
+      GPIO.output(IO_DOWN, GPIO.LOW)
 
 def getBtnName(btn):
   if btn == BTN_START:
@@ -82,6 +149,9 @@ def pressButton(btn):
     btnValue = pad[btn]
     btnName = getBtnName(btnValue)
     print(f"Joystick button {btnName} pressed.")
+    btnIo = getBtnGpio(btnValue)
+    if btnIo > 0:
+      GPIO.output(btnIo, GPIO.HIGH)
   except KeyError:
     print(f"Not managed key {btn}")
 
@@ -90,6 +160,9 @@ def releaseButton(btn):
     btnValue = pad[btn]
     btnName = getBtnName(btnValue)
     print(f"Joystick button {btnName} released.")
+    btnIo = getBtnGpio(btnValue)
+    if btnIo > 0:
+      GPIO.output(btnIo, GPIO.LOW)
   except KeyError:
     print(f"Not managed key {btn}")
 
@@ -99,6 +172,7 @@ def manageAxis(axis, value):
     btnValue = pad[value]
     action = getAxisAction(btnValue)
     print(f"{action}")
+    performIoAction(btnValue)
   except KeyError:
     print(f"Not managed axis {value}")
 
@@ -122,6 +196,7 @@ def main():
   print("!!!!!!!!!!!!!! START !!!!!!!!!!!!!!")
 
   GPIO.setmode(GPIO.BOARD )
+  initGpio()
   # Read pad adapter
   readPadAdapter()
 
